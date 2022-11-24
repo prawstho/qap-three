ded965cd03ae1b15d208e9131ddbef7bda67378b
prawstho <peter.rawsthorne@keyin.com>
2022-11-24 09:55:25 -0330

    basic site working with SQL CREATE

---
+++ SQL/award.sql
@@ -0,0 +1,16 @@
+-- Table: public.award
+
+DROP TABLE IF EXISTS public.award;
+
+CREATE TABLE IF NOT EXISTS public.award
+(
+    award_id serial NOT NULL,
+    name character varying(24) COLLATE pg_catalog."default" NOT NULL,
+    last_update timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT award_pkey PRIMARY KEY (award_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.award
+    OWNER to postgres;
